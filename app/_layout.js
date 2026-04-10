4ce38a817d8dad275d60bf99c43a7e1791986cbb
Victor <victorfreiremartins2@gmail.com>
2026-04-10 15:07:40 -0300

    Corrigindo erro de itens cadastrados

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,20 @@
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function RootLayout() {
+  useEffect(() => {
+    const limparItens = async () => {
+      try {
+        await AsyncStorage.removeItem('itens');
+        console.log('Itens cadastrados foram apagados ao abrir o app.');
+      } catch (error) {
+        console.log('Erro ao limpar itens:', error);
+      }
+    };
+
+    limparItens();
+  }, []);
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
